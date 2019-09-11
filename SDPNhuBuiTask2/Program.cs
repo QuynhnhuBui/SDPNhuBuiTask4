b69b0064571aa55eedf6c565cf5c636e84bab6ea
Kröger Joni-Pekka <ojokr002@edu.xamk.fi>
2019-09-11 12:22:37 +0300

    First modifcation

--- SDPNhuBuiTask2/Program.cs
+++ SDPNhuBuiTask2/Program.cs
@@ -10,6 +10,13 @@ namespace SDPNhuBuiTask2
             Console.WriteLine("This is the very beginning");
             Console.WriteLine("First commit yakes place here");
             Console.WriteLine("This is master branch");
+
+            Console.WriteLine("First modification");
+            Console.WriteLine("This is master branch");
+            Console.WriteLine("Socond commit");
+
+
+           
         }
     }
 }
