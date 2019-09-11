892673c78aefcb31723d6c0f882de116f6bff7b0
Kröger Joni-Pekka <ojokr002@edu.xamk.fi>
2019-09-11 12:26:44 +0300

    New commit

--- SDPNhuBuiTask2/Program.cs
+++ SDPNhuBuiTask2/Program.cs
@@ -15,8 +15,11 @@ namespace SDPNhuBuiTask2
             Console.WriteLine("This is master branch");
             Console.WriteLine("Socond commit");
 
+            Console.WriteLine("This is feature A");
+            Console.WriteLine("This is  FirstBranch");
+            Console.WriteLine("Socond commit");
 
-           
+
         }
     }
 }
